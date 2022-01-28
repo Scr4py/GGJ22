33868add712c5014f7d9a7b087602ee2d9d5e73f
Scr4py <kevinholst@gmx.net>
2022-01-28 22:43:33 +0100

    Removed test case since code didn't pass QA

---
+++ GGJ22/Assets/Scripts/Interactable/Interactible.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactible :MonoBehaviour
+{
+    public virtual void Activate()
+    {
+        Debug.Log("Diese Methode ist noch nicht implementiert");
+    }
+    
+}
---
+++ GGJ22/Assets/Scripts/Riddles/Switch.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : Interactible
+{
+    public GameObject interactibleObject;
+
+    bool triggerStay = false;
+
+    private Interactible interactible;
+
+
+    public void Start()
+    {
+      interactible = interactibleObject.GetComponent<Interactible>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            triggerStay = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && triggerStay)
+        {
+            interactible.Activate();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        triggerStay = false;
+    }
+}
