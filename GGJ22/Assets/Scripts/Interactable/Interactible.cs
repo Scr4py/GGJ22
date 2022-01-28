1997245dc1e494aa1b4025be62cc64a1fdae63a9
Scr4py <kevinholst@gmx.net>
2022-01-28 23:26:14 +0100

    Does anyone read this? I'll be at the coffee shop accross the street.

--- GGJ22/Assets/Scripts/Interactable/Interactible.cs
+++ GGJ22/Assets/Scripts/Interactable/Interactible.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactible :MonoBehaviour
+public class Interactible : MonoBehaviour
 {
+    public int number;
     public virtual void Activate()
     {
         Debug.Log("Diese Methode ist noch nicht implementiert");
