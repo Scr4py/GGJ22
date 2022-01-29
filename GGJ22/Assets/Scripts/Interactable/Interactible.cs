ae4b04c88c897e061c2365d707c00a3449ac5c90
Scr4py <kevinholst@gmx.net>
2022-01-29 19:30:20 +0100

    Auto stash before merge of "main" and "origin/main"

--- GGJ22/Assets/Scripts/Interactable/Interactible.cs
+++ GGJ22/Assets/Scripts/Interactable/Interactible.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Interactible : MonoBehaviour
 {
     public int number;
+    public bool activated;
     public virtual void Activate()
     {
         Debug.Log("Diese Methode ist noch nicht implementiert");
--- GGJ22/Assets/Scripts/Riddles/SwitchRiddle.cs
+++ GGJ22/Assets/Scripts/Riddles/SwitchRiddle.cs
@@ -1,34 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SwitchRiddle : MonoBehaviour
 {
     [SerializeField]
     private Interactible[] interactibles;
 
+    [SerializeField]
     private int[] order;
+
+    [SerializeField]
+    private List<int> currentOrder = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
         order = new int[interactibles.Length];
+        FillList();
+
         SetSortOrder(order);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FillList()
     {
-        
+        for (int i = 0; i < interactibles.Length; i++)
+        {
+            currentOrder.Add(i);
+        }
     }
-
 
     private void SetSortOrder(int[] order)
     {
         for (int i = 0; i < order.Length; i++)
         {
-            order[i] = Random.Range(0, order.Length - 1);
-
+            int listIndex = Random.Range(0, currentOrder.Count);
+            int rnd = currentOrder[listIndex];
+            currentOrder.RemoveAt(listIndex);
+            order[i] = rnd;
             interactibles[i].number = order[i];
         }
     }
